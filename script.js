d9416d1190cadcdf549a7d12fe734f784e9ac8e8
roman3857 <romanshakhrai6@gmail.com>
2026-02-02 19:14:49 +0100

    ccommit

---
+++ script.js
@@ -0,0 +1,3 @@
+const messege1 = prompt("напишіть перше повідомлення!");
+const messege2 = prompt("напишіть друге повідомлення!");
+(console, log((messege1, messege2 = " " && "Обидва поля заповнені" || messege1, messege2 = )));
